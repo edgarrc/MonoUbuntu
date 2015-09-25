d7c2426aeb30695df8278e00bc721d69ec16e4bd
Edgar <edgarrc@users.noreply.github.com>
2015-09-25 14:28:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ echo "##INF:[01/03] Installing apache"
 echo ""
 echo "##INF:[02/03] Installing MONO/Mod-mono"
 
-	#Add the last oficial repository
+	#Add the last official repository
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 	echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 	echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
