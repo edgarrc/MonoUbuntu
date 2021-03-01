0647f9b46c8961e4429e0bdb0b9a40838fa1d094
Edgar <edgarrc@users.noreply.github.com>
2021-03-01 12:13:36 -0300

    hotfix: update for 20.04 server

--- install.sh
+++ install.sh
@@ -66,14 +66,12 @@ echo ""
 echo "##INF:[02/03] Installing MONO/Mod-mono"
 
 	#Add the last official repository
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-	echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
-
-	apt-get update
+	apt install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
 	apt-get -y install mono-runtime
 	apt-get -y install libapache2-mod-mono
-	#apt-get -y install mono-apache-server2
+	#apt-get -y install mono-apache-server4	
 
 	#Disable apache KeepAlive as recomended by mono-project for production use
 	find /etc/apache2/ -name apache2.conf -type f -exec sed -i s/"KeepAlive On"/"KeepAlive Off"/g {} \;
